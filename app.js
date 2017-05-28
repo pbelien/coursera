5779a9d0f7a96bb961040627ff8c8285f26e5c0e
pimbelien <pbelien@sogyo.nl>
2017-05-28 15:58:39 +0200

    change for assignment1

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 (function(){
   'use strict';
-  angular.module('myApp',[])
+  angular.module('LunchCheck',[])
 
-  .controller('myController', function ($scope){
-    $scope.name = 'Pim';
-  });
+  .controller('LunchController', LunchController);
+
+  LunchController.$inject = ['$scope'];
+  function LunchController($scope){
+    $scope.name="";
+
+    $scope.ValidateInput = function(){ //validate if input is give
+      var list = $scope.name
+      if (list== ""){
+        $scope.message = "Please enter data first"
+      } else {
+        EvaluateList(list);
+      };
+    }
+    var ParseInput = function(input){ //returns list lenght
+      console.log(input);
+      return input.split(",").length;
+    };
+
+    var EvaluateList = function(list){ //evaluate and set message
+      var listLenght = ParseInput(list);
+      console.log(listLenght);
+      if (listLenght <= 3) {
+        $scope.message = 'Enjoy!'
+      } else{
+        $scope.message = 'Too much!'
+      };
+    };
+  };
 })();
